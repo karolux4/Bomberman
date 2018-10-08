1b7cb51a3abbefef840d91f648c077b049d077bc
karolux4 <karolisbutkus4@gmail.com>
2018-10-08 10:06:13 +0300

    Fixed bomb in the box bug

---
+++ Bomberman/Assets/Player_Code/Bomb_explosion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb_explosion : MonoBehaviour {
+    public GameObject creator { get; set; }
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Explosion());
+	}
+    private IEnumerator Explosion()
+    {
+        this.gameObject.layer = 11;
+        yield return new WaitForSeconds(1);
+        creator.GetComponent<Shooting_physics>().allowed_to_throw = true;
+        yield return new WaitForSeconds(2);
+        creator.GetComponent<Shooting_physics>().count--;
+        Destroy(this.gameObject);
+    }
+}
---
+++ Bomberman/Assets/Player_Code/Bomb_spawn_collision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb_spawn_collision : MonoBehaviour {
+    public GameObject creator { get; set; }
+    public bool collided { get; set; }
+    private void Start()
+    {
+        collided = false;
+        StartCoroutine(CheckForCollision());
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        collided = true;
+    }
+    private IEnumerator CheckForCollision()
+    {
+        yield return null;
+        this.gameObject.GetComponent<Transform>().position = this.gameObject.GetComponent<Transform>().position + Vector3.zero;
+        yield return null;
+        Debug.Log(collided);
+        if (collided)
+        {
+            Debug.Log(this.gameObject.GetComponent<Transform>().localPosition);
+            Debug.Log(creator.GetComponent<Transform>().localPosition);
+            Destroy(this.gameObject);
+            creator.GetComponent<Shooting_physics>().allowed_to_throw = true;
+            creator.GetComponent<Shooting_physics>().count--;
+        }
+        else
+        {
+            Debug.Log(this.gameObject.GetComponent<Transform>().localPosition);
+            Debug.Log(creator.GetComponent<Transform>().localPosition);
+            this.gameObject.AddComponent<Bomb_explosion>();
+            this.gameObject.GetComponent<Bomb_explosion>().creator = creator;
+        }
+    }
+}
