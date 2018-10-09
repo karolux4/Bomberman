40d8bb4063114ae88a73e9ab1af64f85d2b9b033
karolux4 <karolisbutkus4@gmail.com>
2018-10-09 16:25:51 +0300

    Added boxes destruction

--- Bomberman/Assets/Player_Code/Bomb_explosion.cs
+++ Bomberman/Assets/Player_Code/Bomb_explosion.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bomb_explosion : MonoBehaviour {
     public GameObject creator { get; set; }
+    public float explosion_distance { get; set; }
     // Use this for initialization
     void Start () {
         StartCoroutine(Explosion());
@@ -14,7 +15,50 @@ public class Bomb_explosion : MonoBehaviour {
         yield return new WaitForSeconds(1);
         creator.GetComponent<Shooting_physics>().allowed_to_throw = true;
         yield return new WaitForSeconds(2);
+        Explode();
         creator.GetComponent<Shooting_physics>().count--;
         Destroy(this.gameObject);
+    }
+    void Explode()
+    {
+        explosion_distance = 1;
+        RaycastHit Front, Back, Left, Right;
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),out Front,Mathf.Infinity))
+        {
+            float distance1 = Front.distance;
+            if ((distance1 < explosion_distance)&&(Front.collider.tag == "Boxes"))
+            {
+                Debug.Log("Hit front");
+                Destroy(Front.collider.gameObject.GetComponentInParent<BoxCollider>().gameObject);
+            }
+        }
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out Back))
+        {
+            float distance1 = Back.distance;
+            if ((distance1 < explosion_distance) && (Back.collider.tag == "Boxes"))
+            {
+                Debug.Log("Hit back");
+                Destroy(Back.collider.gameObject.GetComponentInParent<BoxCollider>().gameObject);
+            }
+        }
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out Left))
+        {
+            float distance1 = Left.distance;
+            if ((distance1 < explosion_distance) && (Left.collider.tag == "Boxes"))
+            {
+                Debug.Log("Hit left");
+                Destroy(Left.collider.gameObject.GetComponentInParent<BoxCollider>().gameObject);
+            }
+        }
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out Right))
+        {
+            float distance1 = Right.distance;
+            if ((distance1 < explosion_distance) && (Right.collider.tag == "Boxes"))
+            {
+                Debug.Log("Hit right");
+                Destroy(Right.collider.gameObject.GetComponentInParent<BoxCollider>().gameObject);
+            }
+        }
+
     }
 }
